91aba9e5dac92bc695f936c1e0741737d3e728ca
Uladzimir <okulovitch.vladmir@yandex.ru>
2022-11-19 20:53:42 +0300

    add core_ui module for compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     kapt(DaggerHilt.hiltCompiler)
 
     implementation(project(Modules.core))
+    implementation(project(Modules.coreUi))
     implementation(project(Modules.newsData))
     implementation(project(Modules.newsDomain))
     implementation(project(Modules.newsPresentation))
--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -2,7 +2,7 @@ object Modules {
     const val app = ":app"
 
     const val core = ":core"
-    const val coreUi = ":core-ui"
+    const val coreUi = ":core_ui"
 
     const val newsData = ":news:news_data"
     const val newsDomain = ":news:news_domain"
